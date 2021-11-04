ec46554938768d9784359e214e1e1025fdb27f98
soup <64437861+Fish-game-dev@users.noreply.github.com>
2021-11-04 19:33:45 -0400

    Add files via upload

--- Game Project/entitys/Help/Hint.gd
+++ Game Project/entitys/Help/Hint.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+export(String, MULTILINE) var text
+
 func _ready():
-	pass
+	$ColorRect/Label.text = text
 	
 func _on_Hint_body_entered(body):
 	if (body.get_name() == "Player"):
--- Game Project/entitys/player/Player.gd
+++ Game Project/entitys/player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 export (int) var speed = 600
 export (int) var jump_speed = -1000
 export (int) var gravity = 4000
+export (int) var inertia = 100
 
 var velocity = Vector2.ZERO
 
@@ -16,7 +17,12 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide(velocity, Vector2.UP, false, 4 ,PI/4, false)
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
+	
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("bodies"):
+			collision.collider.apply_central_impulse(-collision.normal * inertia)
